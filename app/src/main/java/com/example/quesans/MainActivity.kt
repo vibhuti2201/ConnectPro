213a6e653cb6155345fd81070e7c15b26ba2f79a
Vibhuti Kapoor <vibhutikapoor22@gmail.com>
2023-06-17 23:45:41 +0530

    Commit

--- app/src/main/java/com/example/quesans/MainActivity.kt
+++ app/src/main/java/com/example/quesans/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.quesans
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var signOutBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        signOutBtn=findViewById(R.id.signOut)
+
+        signOutBtn.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this,VerificationActivity::class.java))
+        }
+
     }
 }
